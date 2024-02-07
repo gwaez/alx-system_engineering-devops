d335bdd1d8b4c77b8b38df68c398be030b591be3
Ahmed Mahmoud <eng.ahmed18@gmail.com>
2024-02-07 07:35:26 -0800

    Gwz0x06

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,2 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/^h.n$/).join#!/usr/bin/env rubyputs ARGV[0].scan(/^h.n$/).join#!/usr/bin/env rubyputs ARGV[0].scan(/^h.n$/).join#!/usr/bin/env rubyputs ARGV[0].scan(/^h.n$/).join#!/usr/bin/env rubyputs ARGV[0].scan(/^h.n$/).join#!/usr/bin/env rubyputs ARGV[0].scan(/^h.n$/).join#!/usr/bin/env rubyputs ARGV[0].scan(/^h.n$/).join#!/usr/bin/env rubyputs ARGV[0].scan(/^h.n$/).join
